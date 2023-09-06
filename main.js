460f146b5f577b2ca8a3557b00fab49eeb7caea0
monmac <monmac@MacBook-Pro-de-monmac.local>
2023-09-06 12:46:31 +0100

    Create pages

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("Hello World!");
+(()=>{"use strict";let e=document.createElement("div");e.innerText="This is main page";const t=e;document.createElement("div").innerText="This is contact page.",document.createElement("div").innerText="This is menu page.",document.querySelector("#content").appendChild(t)})();
